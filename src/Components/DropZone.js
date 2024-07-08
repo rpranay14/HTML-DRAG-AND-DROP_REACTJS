893566cb5c736b225564589d6b19b19ad382fe6c
Pranay Rawat <121486679+rpranay14@users.noreply.github.com>
2024-07-08 16:30:56 +0530

    dropzone styling update

--- src/Components/DropZone.js
+++ src/Components/DropZone.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DropZone = ({data,setDroppedOnZone}) => {
+  const [showArea,setShowArea]=useState(false)
     const handleDrop=(e)=>{
+      setShowArea(false)
         console.log(data,'drop');
         setDroppedOnZone(data)
 
     }
   return (
-    <div onDragOver={(e)=>e.preventDefault()}  onDrop={(e)=>handleDrop(e)} className='h-12 bg-white text-black'>DropZone</div>
+    <div onDragLeave={()=>setShowArea(false)} onDragEnter={()=>setShowArea(true)} onDragOver={(e)=>e.preventDefault()}  onDrop={(e)=>handleDrop(e)} className={`${showArea ?'py-8 opacity-100 bg-gray-500' :'py-0 opacity-0'} h-6 transition-[padding,opacity] border-gray-500 border-dashed border-2 rounded-xl  text-black`}></div>
   )
 }
 
